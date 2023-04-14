be608db2e4518cfbb55c5413727120962ae3066f
KanedaAkira91 <48135777+KanedaAkira91@users.noreply.github.com>
2023-04-14 05:20:23 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,17 @@
             return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
         }
 
+        normalize() {
+            const vectorLength = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+            this.x = this.x / vectorLength;
+            this.y = this.y / vectorLength;
+        }
+
+        normalized() {
+            const vectorLength = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+            return new Vector2D(this.x / vectorLength, this.y / vectorLength);
+        }
+
         clone() {
             return Vector2D.from(this);
         }
@@ -44,6 +55,10 @@
         multiply(value) {
             this.x *= value;
             this.y *= value;
+        }
+
+        multiplied(factor) {
+            return new Vector2D(this.x * factor, this.y * factor);
         }
 
         equals(vector2D) {
@@ -118,16 +133,10 @@
         constructor(id, startingPoint, targetPoint, direction) {
             super(id, targetPoint);
             this.startingPoint = startingPoint;
+            this.currentPoint = startingPoint.clone();
             this.direction = direction;
         }
     }
-
-    /*class InterceptorMissile extends Missile {
-    }
-
-    class EnemyMissile extends Missile {
-
-    }*/
 
     class Base extends Town {
         missileKey;
@@ -167,6 +176,9 @@
             this.calculateCanvasDimensions();
             this.generateTowns();
             this.generateBase();
+            const missilesPerLevel = 100;
+            this.maxMissiles = missilesPerLevel;
+            this.remainingAvailableMissiles = missilesPerLevel;
             this.generateRandomEnemyMissiles();
             this.gameLoop();
         },
@@ -180,20 +192,31 @@
                 this.drawLine(targetCross.topPoint.x, targetCross.topPoint.y, targetCross.bottomPoint.x, targetCross.bottomPoint.y);
             },
             generateRandomEnemyMissiles() {
-                for (let i = 1; i < 10; i++) {
-                    this.generatedEnemyMissiles.push(this.generateRandomEnemyMissile(i));
+                const maxAttackingMissilesCount = 10;
+                const maxAllowedAttackingMissiles = maxAttackingMissilesCount - this.generatedEnemyMissiles.length;
+                const actuallyCreatableMissiles = Math.min(this.remainingAvailableMissiles, maxAllowedAttackingMissiles);
+                const randomAmountOfMissiles = Math.floor(Math.random() * (actuallyCreatableMissiles + 1));
+                //console.log(randomAmountOfMissiles);
+                for (let i = 0; i < randomAmountOfMissiles; i++) {
+                    this.generatedEnemyMissiles.push(this.generateRandomEnemyMissile(Date.now() + i));
                 }
+                //console.log("allowed " + actuallyCreatableMissiles);
+                //console.log("created " + randomAmountOfMissiles);
+                //console.log("remaining " + this.remainingAvailableMissiles);
+                //console.log("");
+                setTimeout(this.generateRandomEnemyMissiles, 500);
             },
             generateRandomEnemyMissile(id) {
+                this.remainingAvailableMissiles -= 1;
                 const targetPoint = this.getRandomTownOrBase().centerTopPoint.clone();
                 const startingPoint = new Vector2D(this.missileCommandCanvas.width * Math.random(), 0);
-                return new EnemyMissile(id, startingPoint, targetPoint, this.calculateDirectionVector(startingPoint, targetPoint));
+                const directionVector = this.calculateDirectionVector(startingPoint, targetPoint);
+                return new EnemyMissile(id, startingPoint, targetPoint, directionVector);
             },
             selectRandomTownForAttack() {
                 const aliveTowns = this.towns.filter(town => !town.destroyed);
                 return aliveTowns[this.getRandomIndex(aliveTowns.length)];
             },
-
             selectRandomBaseForAttack() {
                 const aliveBases = this.bases.filter(base => !base.destroyed);
                 return aliveBases[this.getRandomIndex(aliveBases.length)];
@@ -232,17 +255,16 @@
             },
             handlePlayerMissileFire(pushedKey) {
                 const responsibleBase = this.bases.find(base => base.missileKey === pushedKey);
-                const notFiredMissiles = this.playerMissiles.filter(missile => !missile.startingPoint);
-                if (notFiredMissiles.length === 0) {
+                if (this.playerMissilesBeforeStart.length === 0) {
                     return;
                 }
 
-                const selectedMissile = this.selectNextNotFiredMissile(notFiredMissiles);
+                const selectedMissile = this.selectNextNotFiredMissile();
                 this.prepareMissileForLaunch(selectedMissile, responsibleBase);
             },
-            selectNextNotFiredMissile(notFiredMissiles) {
-                let selectedMissile = notFiredMissiles[0];
-                notFiredMissiles.forEach(notFiredMissile => {
+            selectNextNotFiredMissile() {
+                let selectedMissile = this.playerMissilesBeforeStart[0];
+                this.playerMissilesBeforeStart.forEach(notFiredMissile => {
                     if (notFiredMissile.missileCounter < selectedMissile.missileCounter) {
                         selectedMissile = notFiredMissile;
                     }
@@ -253,42 +275,103 @@
                 selectedMissile.startingPoint = responsibleBase.centerTopPoint.clone();
                 selectedMissile.currentPoint = responsibleBase.centerTopPoint.clone();
                 selectedMissile.direction = this.calculateDirectionVector(selectedMissile.startingPoint, selectedMissile.targetPoint);
+
+                this.playerMissilesBeforeStart = this.playerMissilesBeforeStart.filter(missile => missile !== selectedMissile);
+                this.playerMissiles.push(selectedMissile);
             },
             calculateDirectionVector(startingPoint, targetPoint) {
                 const rawDirectionVector = new Vector2D(targetPoint.x - startingPoint.x, targetPoint.y - startingPoint.y);
-                const vectorLength = Math.sqrt(Math.pow(rawDirectionVector.x, 2) + Math.pow(rawDirectionVector.y, 2));
-                return new Vector2D(rawDirectionVector.x / vectorLength, rawDirectionVector.y / vectorLength);
+                return rawDirectionVector.normalized();
             },
             onMissileTargetClicked(mouseEvent) {
                 const targetPoint = new Vector2D(mouseEvent.offsetX, mouseEvent.offsetY);
                 const targetCross = new TargetCross(targetPoint, this.missileCommandCanvas.width * 0.003);
-                this.playerMissiles.push(new InterceptorMissile(this.missileCounter, targetPoint, targetCross));
+                this.playerMissilesBeforeStart.push(new InterceptorMissile(this.missileCounter, targetPoint, targetCross));
                 this.missileCounter += 1;
+            },
+            hasMissileReachedTarget(missile) {
+                return missile.targetPoint.equals(missile.currentPoint);
+            },
+            hasEnemyMissileReachedTarget(enemyMissile) {
+                return enemyMissile.targetPoint.y <= enemyMissile.currentPoint.y;
+            },
+            missileHasNotReachedTarget(currentDistanceOfMissileToTargetPoint, missile) {
+                return currentDistanceOfMissileToTargetPoint > missile.direction.multiplied(this.dev_InterceptorMissileFactor).length()
             },
             calculatePlayerMissilesMovement() {
                 const firedMissiles = this.playerMissiles.filter(missile => missile.startingPoint && missile.targetPoint && missile.direction && !missile.readyToExplode);
-                firedMissiles.forEach(missile => {
-                    const distanceVector = new Vector2D(missile.targetPoint.x - missile.currentPoint.x, missile.targetPoint.y - missile.currentPoint.y);
-                    const distanceBetweenTPandCP = distanceVector.length();
-                    if (missile.targetPoint.equals(missile.currentPoint)) {
-                        missile.readyToExplode = true;
-                        return;
-                    }
+                firedMissiles.forEach(this.calculatePlayerMissileMovement);
+            },
+            calculatePlayerMissileMovement(missile) {
+                const currentDistanceOfMissileToTargetVector = new Vector2D(missile.targetPoint.x - missile.currentPoint.x, missile.targetPoint.y - missile.currentPoint.y);
+                if (this.hasMissileReachedTarget(missile)) {
+                    missile.readyToExplode = true;
+                    return;
+                }
 
-                    if (distanceBetweenTPandCP > missile.direction.length()) {
-                        missile.currentPoint.add(missile.direction);
-                        return;
-                    }
+                if (this.missileHasNotReachedTarget(currentDistanceOfMissileToTargetVector.length(), missile)) {
+                    missile.currentPoint.add(missile.direction.multiplied(this.dev_InterceptorMissileFactor));
+                    return;
+                }
 
-                    missile.currentPoint.add(distanceVector);
-                    // ist die länge des nächsten schritts kleiner als der abstand von currentpoint zu targetpoint
-                    //if(missile.currentPoint.x === missile.targetPoint.x)
+                missile.currentPoint.add(currentDistanceOfMissileToTargetVector);
+            },
+            hasTownBeenHit() {
+               const explodingEnemyMissiles = this.generatedEnemyMissiles.filter(missile => missile.readyToExplode);
+               explodingEnemyMissiles.forEach(explodingEnemyMissile => {
+                   this.towns.forEach(town => {
+                       const distanceBetweenMissileAndTown = new Vector2D(Math.abs(explodingEnemyMissile.currentPoint.x - town.centerTopPoint.x),Math.abs(explodingEnemyMissile.currentPoint.y - town.centerTopPoint.y)).length();
+                       if(distanceBetweenMissileAndTown <= explodingEnemyMissile.explosionRadius){
+                           town.readyToExplode = true;
+                           this.towns = this.towns.filter(town => !town.readyToExplode)
+                       }
+                   })
+               })
+           },
+          hasBaseBeenHit() {
+               const explodingEnemyMissiles = this.generatedEnemyMissiles.filter(missile => missile.readyToExplode);
+               explodingEnemyMissiles.forEach(explodingEnemyMissile => {
+                   this.bases.forEach(base => {
+                       const distanceBetweenMissileAndTown = new Vector2D(Math.abs(explodingEnemyMissile.currentPoint.x - base.centerTopPoint.x),Math.abs(explodingEnemyMissile.currentPoint.y - base.centerTopPoint.y)).length();
+                       if(distanceBetweenMissileAndTown <= explodingEnemyMissile.explosionRadius){
+                           base.readyToExplode = true;
+                           this.bases = this.bases.filter(base => !base.readyToExplode)
+                       }
+                   })
+               })
+           },
+            isGameOver() {
+                if(this.towns.length < 1) {
+                    console.log("GAME OVER")
+                }
+            },
+            detectMissileCollision() {
+                const explodingPlayerMissiles = this.playerMissiles.filter(missile => missile.readyToExplode);
+                explodingPlayerMissiles.forEach(explodingPlayerMissile => {
+                    this.generatedEnemyMissiles.forEach(enemyMissile => {
+                        const distanceBetweenMissiles = new Vector2D(Math.abs(enemyMissile.currentPoint.x - explodingPlayerMissile.targetPoint.x),Math.abs(enemyMissile.currentPoint.y - explodingPlayerMissile.targetPoint.y)).length();
+                        if(distanceBetweenMissiles <= explodingPlayerMissile.explosionRadius){
+                            //enemyMissile.readyToExplode = true; überlegen warum das so nicht geht
+                            this.generatedEnemyMissiles = this.generatedEnemyMissiles.filter(missile => missile !== enemyMissile);
+                        }
+                    })
                 });
             },
-            drawPlayerMissiles() {
-                const firedMissiles = this.playerMissiles.filter(missile => missile.startingPoint && missile.targetPoint && missile.direction && !missile.readyToExplode);
+            calculateEnemyMissileMovement(enemyMissile) {
+                if (this.hasEnemyMissileReachedTarget(enemyMissile)) {
+                   enemyMissile.readyToExplode = true;
+                    return
+                }
+                enemyMissile.currentPoint.add(enemyMissile.direction);
+            },
+            calculateEnemyMissilesMovement() {
+                const firedEnemyMissiles = this.generatedEnemyMissiles.filter(enemyMissiles => !enemyMissiles.readyToExplode);
+                firedEnemyMissiles.forEach(this.calculateEnemyMissileMovement)
+            },
+            drawMissiles(missiles, color) {
+                const firedMissiles = missiles.filter(missile => !missile.readyToExplode);
                 firedMissiles.forEach(missile => {
-                    this.drawLine(missile.startingPoint.x, missile.startingPoint.y, missile.currentPoint.x, missile.currentPoint.y)
+                    this.drawLine(missile.startingPoint.x, missile.startingPoint.y, missile.currentPoint.x, missile.currentPoint.y, color)
                 });
             },
             drawRectangle(x, y, w, h, color = "white") {
@@ -315,31 +398,34 @@
                 this.drawingContext2D.strokeStyle = color;
                 this.drawingContext2D.stroke();
             },
-            calculateExplosionRadiuses() {
+            isMissileExplosionFinished(explodingMissile) {
                 const maxExplosionRadiusPixel = EXPLOSION_RADIUS_MAX * this.missileCommandCanvas.width;
-                this.playerMissiles
-                    .filter(m => m.readyToExplode)
+                return maxExplosionRadiusPixel < explodingMissile.explosionRadius;
+            },
+            calculateExplosionRadiuses(missiles) {
+                const explodingMissiles = missiles.filter(m => m.readyToExplode);
+                explodingMissiles
                     .forEach(explodingMissile => {
-                            if (maxExplosionRadiusPixel >= explodingMissile.explosionRadius) {
-                                explodingMissile.explosionRadius += 1;
+                            if (this.isMissileExplosionFinished(explodingMissile)) {
+                                const explodedMissileIndex = missiles.indexOf(explodingMissile);
+                                missiles.splice(explodedMissileIndex, 1);
                                 return;
                             }
-                            this.playerMissiles = this.playerMissiles.filter(missile => missile !== explodingMissile);
+                            //still exploding
+                            explodingMissile.explosionRadius += 1;
                         }
                     );
             },
-            drawExplosions() {
-                this.playerMissiles
+            drawExplosions(missiles, color) {
+                missiles
                     .filter(missile => missile.readyToExplode)
-                    .forEach(missile => this.drawCircle(missile.targetPoint, missile.explosionRadius))
-
+                    .forEach(missile => this.drawCircle(missile.targetPoint, missile.explosionRadius, color))
             },
             drawTargetCrosses() {
-                this.playerMissiles.filter(missile => !missile.readyToExplode).forEach(missile => this.drawTargetCross(missile.targetCross));
-
-            },
-            generateRandomMissiles() {
-                this.drawLine(this.missileCommandCanvas.width * Math.random(), 0, this.missileCommandCanvas.width * Math.random(), this.missileCommandCanvas.height)
+                this.playerMissilesBeforeStart.forEach(missile => this.drawTargetCross(missile.targetCross));
+                this.playerMissiles
+                    .filter(missile => !missile.readyToExplode)
+                    .forEach(missile => this.drawTargetCross(missile.targetCross));
             },
             calculateCanvasDimensions() {
                 this.missileCommandCanvas.width = window.innerWidth * 0.70;
@@ -386,17 +472,24 @@
                 this.clearCanvas();
 
                 this.calculatePlayerMissilesMovement();
-                this.calculateExplosionRadiuses();
+                this.calculateEnemyMissilesMovement();
+                this.calculateExplosionRadiuses(this.playerMissiles);
+                this.calculateExplosionRadiuses(this.generatedEnemyMissiles);
 
-                //collisions calculation
+                this.detectMissileCollision();
+
+                this.hasTownBeenHit();
+                this.hasBaseBeenHit();
+
+                this.isGameOver();
 
                 this.drawTowns();
                 this.drawBases();
-                this.drawExplosions();
+                this.drawExplosions(this.playerMissiles, "white");
+                this.drawExplosions(this.generatedEnemyMissiles, "red");
                 this.drawTargetCrosses();
-                this.drawPlayerMissiles();
-
-                //this.generateRandomMissiles();
+                this.drawMissiles(this.playerMissiles, "white");
+                this.drawMissiles(this.generatedEnemyMissiles, "red");
                 setTimeout(this.gameLoop.bind(this), MS_PER_FRAME);
             },
         },
@@ -408,10 +501,13 @@
                 towns: [],
                 bases: [],
                 playerMissiles: [],
+                playerMissilesBeforeStart: [],
                 // playerMissilesGraveyard: [],
                 missileCounter: 0,
-                enemyRockets: [],
                 generatedEnemyMissiles: [],
+                maxMissiles: 10,
+                remainingAvailableMissiles: 10,
+                dev_InterceptorMissileFactor: 100
             }
         },
         computed: {}
